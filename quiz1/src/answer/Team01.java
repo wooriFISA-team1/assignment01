10a775dd4d5e9b4f9c95c688604a1097e2f10c4e
Eun <eungi9640@gmail.com>
2023-04-30 11:25:38 +0900

    답지

--- quiz1/src/answer/Team01.java
+++ quiz1/src/answer/Team01.java
@@ -1,4 +1,4 @@
-package team;
+package answer;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -12,31 +12,39 @@ public class Team01 {
 		Engineer engineer1 = new Engineer("김엔진");
 		Engineer engineer2 = new Engineer("정엔진");
 
-		ArrayList<Benz> benzList = new ArrayList<>();
 
 		Benz benz = new Benz(200, "A-Class");
 		AMG amg = new AMG(300, "G-Class 63 AMG", engineer1);
-
-		//다음 두 줄을 한 줄로 줄여주세요
-		//업캐스팅
-		//AMG temp = new AMG(522, "AMG_GT", engineer1);
-		//Car car = temp;
+		//---------------문제 1---------------
 		Car car = new AMG(522, "AMG_GT", engineer1);
+		//------------------------------------
 
+
+
+		//---------------문제 2---------------
 		//car의 엔지니어를 [정엔진]씨로 바꾸고, 엔지니어의 이름을 출력해주세요
 		((AMG)car).engineer = engineer2;
 		System.out.println("바꾼 엔지니어 이름 => " + ((AMG)car).engineer.name + "\n");
+		//------------------------------------
+
+
+
+		//---------------문제 3---------------
+		//car를 컴파일에러 없이 benzList에 추가하고, 모델명을 출력해주세요
+		//다운캐스팅
+		ArrayList<Benz> benzList = new ArrayList<>();
 
 		benzList.add(benz);
 		benzList.add(amg);
-
-		//car를 컴파일에러 없이 benzList에 추가하고, 모델명을 출력해주세요
-		//다운캐스팅
-		//benzList.add(car);
 		benzList.add((Benz)car);
 		System.out.println("마지막으로 추가한 모델 => " + ((Benz)benzList.get(2)).model + "\n");
+		//------------------------------------
 
-		//논리적 예외 1
+
+
+		//---------------문제 4---------------
+		//논리적 예외 1 -> 예외가 발생하는 경우를 말해주세요
+		//정답 : ArrayList 인덱스 범위를 벗어난 경우, 숫자를 입력하지 않았을 경우
 		System.out.print("몇 번째 차를 조회할까요? : ");
 		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 
@@ -47,13 +55,19 @@ public class Team01 {
 				Benz myBenz = benzList.get(n);
 				System.out.println("모델명 : " + myBenz.model + "\n");
 				flag1 = false;
-			}catch (Exception e) {
-				System.out.print("해당 차가 존재하지 않습니다.\n다시 입력하세요 : ");
+			}catch(NumberFormatException e){
+				System.out.print("숫자를 입력하세요 : ");
+			}catch (IndexOutOfBoundsException e) {
+				System.out.print("해당 차가 존재하지 않습니다. 다시 입력하세요 : ");
 			}
 		}while(flag1);
+		//------------------------------------
 
 
-		//논리적 예외 2
+
+		//---------------문제 5---------------
+		//논리적 예외 2 -> 예외가 발생하는 경우를 말해주세요
+		//정답 : 음수를 입력한 경우, 숫자를 입력하지 않았을 경우
 		boolean flag2 = true;
 		do {
 			try {
@@ -62,10 +76,13 @@ public class Team01 {
 				Benz yourBenz = new Benz(inputHP, "님이 산 차");
 				System.out.println(yourBenz.model + "의 마력은 " + yourBenz.HP + " 입니다.");
 				flag2 = false;
-			}catch(RuntimeException e) {
-				System.out.println("님이 산 차의 " + e.getMessage() + "\n다시 입력하세요.");
+			}catch(NumberFormatException e){
+				System.out.print("숫자를 입력하세요 : ");
+			}catch(Exception e) {
+				System.out.print("님이 산 차의 " + e.getMessage() + "\n다시 입력하세요 : ");
 			}
 		}while(flag2);
+		//------------------------------------
 
 	}
 
